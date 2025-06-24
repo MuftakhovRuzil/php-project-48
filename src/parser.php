2a5f7c8be09a2e783eedb7da0114586e9edb8437
admin <admin@MacBookPro.Dlink>
2025-06-24 23:36:20 +0500

    new age

--- src/parser.php
+++ src/parser.php
@@ -1,29 +1,3 @@
 <?php
 
-namespace GENDIFF\PARSER;
 
-use Symfony\Component\Yaml\Yaml;
-
-function parsingFile(string $filePath)
-{
-    if (!file_exists($filePath)) {
-        throw new \Exception("Invalid filepath: {$filePath}");
-    }
-    $fileContent = file_get_contents($filePath);
-
-    if ($fileContent === false) {
-        throw new \Exception("Can't read file: {$filePath}");
-    }
-
-    $pathInfo = pathinfo($filePath);
-    //var_dump ($pathInfo['extension']);
-
-    switch ($pathInfo['extension']) {
-        case 'json':
-            return json_decode($fileContent);
-        case 'yaml':
-            return Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP);
-        case 'yml':
-            return Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP);
-    }
-}
